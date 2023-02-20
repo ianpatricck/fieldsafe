ec800a786820d2510d47822427bf477cc18a75f3
ianpatricck <ianpatrick499@gmail.com>
2023-02-20 14:32:40 -0300

    :tada: Initial structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# @ FieldSafe
+## Security and persistence of your app data
+
+Library to assist in the development of security for form fields, with validation by RegEx, Sanitization and Mask in inputs.
---
+++ core/fieldsafe.js
@@ -0,0 +1,29 @@
+/*
+ * FieldSafe main script
+ * Author: Ian Patrick
+ *
+ */
+
+class Sanitization {
+
+  email() {}
+  password() {}
+  text() {}
+  cpf() {}
+  cnpj() {}
+  cep() {}
+  cnh() {}
+
+}
+
+class Validation {
+ 
+  email() {}
+  password() {}
+  text() {}
+  cpf() {}
+  cnpj() {}
+  cep() {}
+  cnh() {}
+
+}
