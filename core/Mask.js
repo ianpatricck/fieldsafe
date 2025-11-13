6476dc18db3beb1120f71d007b9a2dff72c8370f
ianpatricck <ianpatrick499@gmail.com>
2025-11-13 02:16:05 -0300

    :recycle: re-created the input mask class

--- core/Mask.js
+++ core/Mask.js
@@ -1,111 +1,55 @@
 class Mask {
-  constructor(formElement = null) {
-    this.formStatus = [];
+  /**
+   * Mask constructor to validate and replace input masks.
+   *
+   * @param {array} inputList
+   *
+   */
+  constructor(inputList) {
+    inputList.forEach((input) => {
+      const inputMask = input.getAttribute("fs-mask");
 
-    if (formElement) {
-      if (typeof formElement !== "object")
-        throw new Error("form element on constructor is not a object");
+      input.addEventListener("input", (event) => {
+        event.target.value = this.#accepted(event.target.value);
 
-      this.#parseMasksToForm(formElement);
-    }
-  }
+        const value = event.target.value;
+        let maskedValue = this.#replaceValues(value, inputMask);
 
-  #parseMasksToForm(form) {
-    for (let input of form) {
-      if (input.getAttribute("fs-mask"))
-        this.#adjustMaskToInput(input, input.getAttribute("fs-mask"));
-    }
-  }
+        if (maskedValue.length == inputMask.length) {
+          event.target.value = maskedValue;
+        }
 
-  #adjustMaskToInput(input, mask) {
-    input.maxLength = mask.length;
-    input.addEventListener("keyup", (e) => {
-      if (input.value.trim() !== "" && e.code !== "Backspace") {
-        let maskSplit = mask.split("");
-        let inputSplit = input.value.split("");
-
-        inputSplit = inputSplit.filter((element) => {
-          if (!maskSplit.includes(element) && element.trim() !== "") {
-            return element;
-          }
-        });
-
-        let textMasked = [];
-
-        maskSplit.forEach((elemMask) => {
-          if (elemMask !== "#") {
-            textMasked.push(elemMask);
-          } else {
-            const findFirst = inputSplit.find((element) => element);
-
-            inputSplit.shift();
-            textMasked.push(findFirst);
-          }
-        });
-
-        input.value = textMasked.join("");
-      }
+        maskedValue = value.length == 0 ? "" : maskedValue;
+      });
     });
   }
 
-  fmatch(input, match, options) {
-    if (!input) throw new Error("input param is not defined");
-
-    if (!match) throw new Error("match param is not defined");
-
-    let { success, failure, response } = options;
-
-    if (!success || !failure || !response)
-      throw new Error("success, failure or response fields are undefined");
-
-    try {
-      input.addEventListener("keyup", (event) => {
-        if (input.value.trim() !== "") {
-          if (typeof match === "string") {
-            if (REGEX[match].test(input.value)) {
-              let newStatus = {
-                name: input.name,
-                success: true,
-                message: failure,
-              };
-
-              this.formStatus = this.formStatus.filter(
-                (status) => status.name !== newStatus.name,
-              );
-              this.formStatus.push(newStatus);
-
-              if (typeof response === "string")
-                document.querySelector(response).innerText = success;
-            } else {
-              let newStatus = {
-                name: input.name,
-                success: false,
-                message: failure,
-              };
-
-              this.formStatus = this.formStatus.filter(
-                (status) => status.name !== newStatus.name,
-              );
-              this.formStatus.push(newStatus);
-
-              if (typeof response === "string")
-                document.querySelector(response).innerText = failure;
-            }
-          }
-        } else {
-          document.querySelector(response).innerText = "";
-        }
-      });
-
-      return this;
-    } catch (err) {
-      throw new Error(`input param is not valid: ${typeof input}`);
-    }
+  /**
+   * Filter only numbers
+   *
+   * @param {string} targetValue
+   * @return {string}
+   */
+  #accepted(targetValue) {
+    return targetValue.replace(/[^0-9]/g, "");
   }
 
-  validate(fn = null) {
-    if (fn) fn(this.formStatus);
-    else return this.formStatus;
+  /**
+   * Transform every single '#' in the mask to a number value.
+   *
+   * @param {string} value
+   * @param {string} inputMask
+   * @return {string}
+   *
+   */
+  #replaceValues(value, inputMask) {
+    let valueCounter = 0;
+    let maskedValue = inputMask.replace(/(#|A)/g, function () {
+      let valueArr = value.split("");
+      return valueArr[valueCounter++] || "";
+    });
+
+    return maskedValue;
   }
 }
 
